5d8a8e355b49a7ca07c8e411b26a86d54929c909
Jessica <letsjesscode@gmail.com>
2021-02-18 12:35:55 -0500

    commit tree 79022975e3541361f12fef9379fa33ad518735a6

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards( nameCard, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < nameCard.length; i++ ) {
+      thankYouCards.push( `Thank you, ${nameCard[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+}
+
+function countDown(num) {
+     while (num > 0){
+            console.log(num);
+            num -= 1;
+        }
+        console.log( num );
+} 
+    
